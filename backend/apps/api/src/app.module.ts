9239baab240ecdc839d2afa35bb864c1a56e0d5d
quassbottle <quassbottleyt@gmail.com>
2025-05-20 23:29:41 +0300

    fix

--- backend/apps/api/src/app.module.ts
+++ backend/apps/api/src/app.module.ts
@@ -3,6 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '@app/db';
 
 @Module({
-  imports: [PrismaModule, ConfigModule.forRoot()],
+    imports: [PrismaModule, ConfigModule.forRoot()],
 })
 export class AppModule {}
