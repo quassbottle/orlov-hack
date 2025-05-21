96c7e775e5e5e98bbb7bf797d9fe229fdcd36138
rh0danthe <rita.pichushkina.04@mail.ru>
2025-05-21 20:42:47 +0300

    fix

--- frontend/app/routes/users.server.ts
+++
@@ -1,9 +0,0 @@
-import { LoaderFunctionArgs } from "@remix-run/node";
-import { UserModel, prisma } from "~/lib/.server/prisma";
-
-export const loader = async ({
-  params,
-  request,
-}: LoaderFunctionArgs): Promise<UserModel[]> => {
-  return prisma.user.findMany();
-};
