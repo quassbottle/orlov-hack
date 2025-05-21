c1a33f3462cb5910c2bfb0d59a79d81386f5db0f
quassbottle <quassbottleyt@gmail.com>
2025-05-21 20:57:02 +0300

    fix

--- frontend/app/routes/login.tsx
+++ frontend/app/routes/login.tsx
@@ -1,40 +1,29 @@
-
 import { Form, useActionData } from "@remix-run/react";
 import { verifyLogin } from "~/lib/.server/api/verifyLogin";
 import { createUserSession } from "~/lib/.server/session";
 
 export async function action({ request }: { request: Request }) {
-    const formData = await request.formData();
-    const username = formData.get("username") as string;
-    const password = formData.get("password") as string;
+  const formData = await request.formData();
+  const username = formData.get("username") as string;
+  const password = formData.get("password") as string;
 
-    const user = await verifyLogin({ username, password });
-    if (!user) {
-        return { error: "Неверный логин или пароль" };
-    }
+  const user = await verifyLogin({ username, password });
+  if (!user) {
+    return { error: "Неверный логин или пароль" };
+  }
 
-    return await createUserSession(String(user.id), "/");
+  return await createUserSession(String(user.id), "/");
 }
 
 export default function Login() {
-    const data = useActionData<typeof action>();
+  const data = useActionData<typeof action>();
 
-    return (
-        <Form method="post">
-            <input
-                name="username"
-                type="text"
-                placeholder="Username"
-                required
-            />
-            <input
-                name="password"
-                type="password"
-                placeholder="Password"
-                required
-            />
-            {data?.error && <p style={{ color: "red" }}>{data.error}</p>}
-            <button type="submit">Войти</button>
-        </Form>
-    );
+  return (
+    <Form method="post">
+      <input name="username" type="text" placeholder="Username" required />
+      <input name="password" type="password" placeholder="Password" required />
+      {data?.error && <p className="text-red">{data.error}</p>}
+      <button type="submit">Войти</button>
+    </Form>
+  );
 }
