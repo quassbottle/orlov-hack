f5353a2e01057d74a9988b2a3d02f579f008a369
cutemiya <k-shaipov@mail.ru>
2025-05-23 05:34:18 +0300

    similarity

--- neuro-client/main.py
+++ neuro-client/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from app import App
 from config.config import parse
+from huggingface_hub import login
 
 
 CONFIG_SRC = '.config/prod.toml'
 
 cfg = parse(CONFIG_SRC)
+login(token=cfg.deep_seek_token)
 app: FastAPI = App(cfg).get_app()
 
 
