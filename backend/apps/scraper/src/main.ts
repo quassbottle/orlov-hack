93cd1b59e85f7510f659ede89e247e58115c648e
cutemiya <k-shaipov@mail.ru>
2025-05-23 05:44:36 +0300

    return

--- backend/apps/scraper/src/main.ts
+++ backend/apps/scraper/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions } from '@nestjs/microservices';
 import { ScraperModule } from './modules/scraper/scraper.module';
 
+
 async function bootstrap() {
     const app =
         await NestFactory.createMicroservice<MicroserviceOptions>(
