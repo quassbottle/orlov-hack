ae760d01ed9b89ce89c0ac8f28fa8931b94fdf97
cutemiya <k-shaipov@mail.ru>
2025-05-23 07:18:25 +0300

    test

--- neuro-client/repository/clickhouse/clickhouse.py
+++ neuro-client/repository/clickhouse/clickhouse.py
@@ -21,10 +21,25 @@ def get_data():
         print(result)
 
         for row in result:
-           res.append(Message(**row)) 
+           res.append(map_message(row))
+
+        return res 
 
     except Exception as e:
         print(f"Ошибка при выполнении запроса: {e}")
 
     finally:
         client.disconnect()
+
+def map_message(row):
+    return Message(
+        uuid=row[0],
+        source=row[1],
+        additional_data=row[2],
+        created_at=row[3],
+        original_text=row[4],
+        post_date=row[5],
+        problem=row[6],
+        location=row[7],
+        problem_date=row[8]
+    )
