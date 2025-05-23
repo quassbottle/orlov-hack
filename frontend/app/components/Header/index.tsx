9524c5e5a239e4db0102d3ca1b57408734627097
Alex <sasha_novosel@mail.ru>
2025-05-23 04:19:45 +0300

    adding Channel

--- frontend/app/components/Header/index.tsx
+++ frontend/app/components/Header/index.tsx
@@ -7,7 +7,6 @@ type IPageButton = {
 
 export const PageButton = (data: IPageButton) => {
   const navigator = useNavigate();
-  console.log(data.path);
   return (
     <button
       className="pb-[12px] text-2xl text-gray-700 font-bold mb-1 hover:text-gray-400"
